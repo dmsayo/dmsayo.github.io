6d59de20926f6e59f09e743961f8bf2db6204b5f
dmsayo <derrylmsayo@gmail.com>
2022-12-15 13:05:01 -0800

    Add more styling to landing section

--- dmsayo.github.io/src/components/Landing/Landing.tsx
+++ dmsayo.github.io/src/components/Landing/Landing.tsx
@@ -1,20 +1,30 @@
 import React from "react"
 import "./Landing.scss";
-import profilePicture from "../Landing/img/pfp_3.jpg";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export const Landing = () => {
     return (
         <div className="landing">
-            <div className="landing_text">
+            <div className="landing__top">
                 <div className="landing__header">
-                    Derryl Sayo
+                    DERRYL SAYO
                 </div>
-                <div className="landing__description">
-                    My name is Derryl Sayo
+                <div className="landing__nav">
+                    <div className="landing__nav-item">
+                        About
+                    </div>
+                    <div className="landing__nav-item">
+                        Projects
+                    </div>
+                    <div className="landing__nav-item">
+                        Contact
+                    </div>
                 </div>
             </div>
-            <div className="landing__image">
-                <img src={profilePicture} alt="" />
+            <div className="landing__bottom">
+                <div>
+                    <ExpandMoreIcon style={{width: "50px", height: "50px"}}/>
+                </div>
             </div>
         </div>
     )
--- dmsayo.github.io/src/components/Navbar/Navbar.tsx
+++ dmsayo.github.io/src/components/Navbar/Navbar.tsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
                     Project
                 </div>
                 <div className="navbar__link-item">
-                    Contact Me
+                    Contact
                 </div>
             </div>
         </div>
