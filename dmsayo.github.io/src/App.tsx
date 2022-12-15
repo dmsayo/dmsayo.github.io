edecb7083a6ca365b29bd63d11835b86f2503482
dmsayo <derrylmsayo@gmail.com>
2022-12-15 01:00:06 -0800

    Start on landing section

--- dmsayo.github.io/src/App.tsx
+++ dmsayo.github.io/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.scss';
+import { Landing } from './components/Landing/Landing';
 import { Navbar } from './components/Navbar/Navbar';
 import { Projects } from './components/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
+      <Landing />
+      <div className="App_projects">
+        Projects
+      </div>
       <Projects />
     </div>
   );
--- dmsayo.github.io/src/components/Landing/Landing.tsx
+++ dmsayo.github.io/src/components/Landing/Landing.tsx
@@ -1,9 +1,21 @@
 import React from "react"
+import "./Landing.scss";
+import profilePicture from "../Landing/img/pfp_3.jpg";
 
 export const Landing = () => {
     return (
-        <div>
-            
+        <div className="landing">
+            <div className="landing_text">
+                <div className="landing__header">
+                    Derryl Sayo
+                </div>
+                <div className="landing__description">
+                    My name is Derryl Sayo
+                </div>
+            </div>
+            <div className="landing__image">
+                <img src={profilePicture} alt="" />
+            </div>
         </div>
     )
 };
