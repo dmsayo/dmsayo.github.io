c2567145762389b9aadc6d7defc382a98317cd2a
dmsayo <derrylmsayo@gmail.com>
2022-12-15 17:29:54 -0800

    Fill out project cards

--- dmsayo.github.io/src/components/Projects/Projects.tsx
+++ dmsayo.github.io/src/components/Projects/Projects.tsx
@@ -9,9 +9,9 @@ export const Projects = () => {
             <div className="projects__title">
                 Projects
             </div>
-            <ProjectCard title="title" imageUrl={""} description="This is the description" moreLink="#"/>
-            <ProjectCard title="title" imageUrl={""} description="This is the description" moreLink="#"/>
-            <ProjectCard title="title" imageUrl={""} description="This is the description" moreLink="#"/>
+            <ProjectCard title="Odonata Keyboard Collection" imageUrl={""} description="The odonata ergonomic keyboard collection, designed from the ground up by Derryl Sayo" moreLink="https://github.com/dmsayo/odonata-keyboard-collection"/>
+            <ProjectCard title="Apartment Database" imageUrl={""} description="A web application modelling an apartment management system written in PHP and using MySQL" moreLink="https://github.com/dmsayo/Apartment-Database"/>
+            <ProjectCard title="Travel Journal" imageUrl={""} description="A desktop GUI application written in Java to blog your travels" moreLink="https://github.com/dmsayo/Travel-Log"/>
         </div>
     )
 };
