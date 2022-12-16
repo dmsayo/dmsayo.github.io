452f6a493eedf78b89f8ed4d81f1dbc818710d14
dmsayo <derrylmsayo@gmail.com>
2022-12-15 17:32:02 -0800

    Add personal website to projects cards

--- dmsayo.github.io/src/components/Projects/Projects.tsx
+++ dmsayo.github.io/src/components/Projects/Projects.tsx
@@ -12,6 +12,7 @@ export const Projects = () => {
             <ProjectCard title="Odonata Keyboard Collection" imageUrl={""} description="The odonata ergonomic keyboard collection, designed from the ground up by Derryl Sayo" moreLink="https://github.com/dmsayo/odonata-keyboard-collection"/>
             <ProjectCard title="Apartment Database" imageUrl={""} description="A web application modelling an apartment management system written in PHP and using MySQL" moreLink="https://github.com/dmsayo/Apartment-Database"/>
             <ProjectCard title="Travel Journal" imageUrl={""} description="A desktop GUI application written in Java to blog your travels" moreLink="https://github.com/dmsayo/Travel-Log"/>
+            <ProjectCard title="dmsayo.github.io" imageUrl={""} description="My personal website (here!). Written using React.js and Typescript. Click learn more to see the full source code." moreLink="https://github.com/dmsayo/Travel-Log"/>
         </div>
     )
 };
