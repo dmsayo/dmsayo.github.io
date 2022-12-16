26d7e511b872110b3b4bf08b219a58e089c2f492
dmsayo <derrylmsayo@gmail.com>
2022-12-15 23:23:25 -0800

    Add contact info

--- dmsayo.github.io/src/components/Contact/Contact.tsx
+++ dmsayo.github.io/src/components/Contact/Contact.tsx
@@ -7,6 +7,24 @@ export const Contact = () => {
             <div className="contact__title">
                 Contact Me
             </div>
+            <div className="contact__body">
+                <div className="contact__body-entry">
+                    <div className="contact__body-entry-name">
+                        Email:
+                    </div>
+                    <div className="contact__body-entry-content">
+                        derrylmsayo@gmail.com
+                    </div>
+                </div>
+                <div className="contact__body-entry">
+                    <div className="contact__body-entry-name">
+                        GitHub:
+                    </div>
+                    <div className="contact__body-entry-content">
+                        <a href="https://github.com/dmsayo">github.com/dmsayo</a>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
